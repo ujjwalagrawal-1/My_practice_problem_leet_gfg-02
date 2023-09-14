eec23141531676cb817d1e706336b309b676e99c
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2023-09-14 22:18:13 +0530

    Time: 4 ms (36.55%), Space: 6.2 MB (44.49%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
-int solve(long n,long count){
-    if(n == 0 || n==1){
-        return 0;
-    }
-    if(n%2 == 1){
-        long x = solve(n+1,count);
-        long y = solve(n-1,count);
-        return min(x,y)+1;
-    }
-    long t = solve(n/2,count++);
-    count += t;
-    return count;
+int recur(long n,long count){
+    // if(n == 0 || n==1){
+    //     return 0;
+    // }
+    // if(n%2 == 1){
+    //     long x = solve(n+1,count);
+    //     long y = solve(n-1,count);
+    //     return min(x,y)+1;
+    // }
+    // long t = solve(n/2,count++);
+    // count += t;
+    // return count;
+    if(n<=1)
+        {return count;}
+        if(n%2==0)
+        {
+            return recur(n/2,++count);
+        }
+        else
+        {
+            return min(recur(n-1,count),recur(n+1,count))+1;
+        }
 }
     int integerReplacement(int n) {
         long count = 0;
-        return solve(n,count);
+        return recur(n,count);
+
+    /*
+    recur(int n,int cnt=0)
+    {
+        if(n<=1)
+        {return count;}
+
+        if(n%2==0)
+        {
+            return recur(n/2,++count);
+        }
+        else
+        {
+            return min(recur(n-1,cnt),recur(n+1,count));
+        }
+    }
+    */
     }
 };
