d11c10755c8ba5d999d52b2d06e8c38175537329
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2023-11-29 00:11:37 +0530

    Time: 12 ms (89.25%) | Memory: 15.3 MB (77.83%) - LeetSync

--- 203-remove-linked-list-elements/remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/remove-linked-list-elements.cpp
@@ -1,31 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        if (head == nullptr) {
+        if(head == NULL){
             return head;
         }
-
-        // Remove elements from the beginning
-        while (head != nullptr && head->val == val) {
+        while(head != NULL && head->val == val){
             head = head->next;
         }
 
         ListNode* temp = head;
         ListNode* x = head;
-
-        while (temp != nullptr) {
-            while (temp != nullptr && temp->val != val) {
+        while(temp != NULL){
+             while (temp != NULL) {
+            while (temp != NULL && temp->val != val) {
                 x = temp;
                 temp = temp->next;
             }
-
-            // Update x->next
-            if (temp != nullptr) {
+            if (temp != NULL) {
                 x->next = temp->next;
                 temp = temp->next;
             }
         }
-
+        }
         return head;
     }
 };
