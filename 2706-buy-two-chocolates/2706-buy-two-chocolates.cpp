d4adae2061a2ebbc0e0e1d771a082fc56a04ecb0
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2023-12-20 10:36:30 +0530

    Time: 17 ms (25.91%), Space: 46.9 MB (58.3%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,9 +1,30 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end());
-        if(prices[0]+prices[1] <= money){
-            return money -prices[0]-prices[1];
+        //Second Approch is by finding two minimum element in the array;
+
+        int first = INT_MIN;
+        int second = INT_MIN;
+        if(prices[0]<=prices[1]){
+            first = prices[0];
+            second = prices[1];
+        }
+        else{
+            first = prices[1];
+            second = prices[0];
+        }
+        for(int i = 2;i<prices.size();i++){
+            if(prices[i]<=first){
+                second = first;
+                first = prices[i];
+            }
+            else if(prices[i]<second){
+                second = prices[i];
+            }
+        }
+        // cout<<first<<" "<<second<<endl;
+        if(first+second <= money){
+            return money - (first+second);
         }
         else{
             return money;
