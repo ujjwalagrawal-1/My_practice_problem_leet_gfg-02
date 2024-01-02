232443a3c4c34aedc6eb2df6ad0e3d3fd38b817e
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-03 04:33:37 +0530

    Time: 142 ms (5.49%), Space: 9 MB (16.13%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
 int mod = 1000000007;
 int recursiverelation(int n,int k,int target){
-    if( n<0){
-        return 0;
-    }
+    
     if(n == 0 && target == 0){
         return 1;
     }
@@ -18,9 +16,6 @@ int recursiverelation(int n,int k,int target){
     return ans;
 }
 int memoisation(int n,int k,int target,vector<vector<int>> &dp){
-    if(target <0 || n < 0){
-        return 0;
-    }
     if(n == 0 && target == 0){
         return 1;
     }
@@ -28,7 +23,7 @@ int memoisation(int n,int k,int target,vector<vector<int>> &dp){
         return 0;
     }
     if(dp[n][target] != -1){
-        return dp[n][target]%mod;
+        return dp[n][target];
     }
 
     int ans = 0;
@@ -41,13 +36,37 @@ int memoisation(int n,int k,int target,vector<vector<int>> &dp){
     dp[n][target] = ans;
     return dp[n][target]%mod;
 }
+int tabulation(int n,int k,int target){
+    vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+    dp[0][0] = 1;
+
+    for(int N = 1;N<=n;N++){
+        for(int j = 1;j<=target;j++){
+            int ans = 0;
+            for(int i = 1;i<=k;i++){
+                int recans = 0;
+                if(j-i>=0)
+                recans += dp[N-1][j-i]%mod;
+                ans = (ans + recans)%mod;
+            }
+            dp[N][j] = ans;
+        }
+    }
+    return dp[n][target];
+}
     int numRollsToTarget(int n, int k, int target) {
         // if(target < 0){
         //     return 0;
         // }
-
         // return recursiverelation(n,k,target);
-        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-        return memoisation(n,k,target,dp);
+
+
+        // // Memoisation concept
+        // vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
+        // return memoisation(n,k,target,dp);
+
+
+        ////////////
+        return tabulation(n,k,target);
     }
 };
