89b1286a73e899c23585aea17d68b27780e0c232
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-03 01:18:33 +0530

    Time: 0 ms (100%), Space: 6.4 MB (65.5%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -37,28 +37,52 @@ int tb(int n){
     }
     return dp[n];
 }
+int sop(int n){
+    int prev3 = 0;
+    int prev2 = 1;
+    int prev1 = 1;
+    for(int j = 3;j<=n;j++){
+        int curr = prev3 + prev2 + prev1;
+        prev3 = prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
     int tribonacci(int n) {
-        // if(n == 0){
-        // return 0;
-        // }
-        // if(n == 1){
-        //     return 1;
-        // }
-        // if(n == 2){
-        //     return 1;
-        // }
-
+        /*
+        if(n == 0){
+        return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 1;
+        }
+        */
         // return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
-
-
         // above one is the basic approch
+        //////////////////////////////////
 
-        ////////////////////////////////////
+
         // by Memoisation  method
         // vector<int> dp(n+1,-1);
         // return Memoisation(n,dp);
         ////////////////////////////////////
+
+
         // By Tabulation method
-        return tb(n);
+        // return tb(n);
+        ///////////////////////
+
+
+        // by Space Optimisation
+        if(n == 0){
+            return 0;
+        }
+        return sop(n);
+
     }
 };
