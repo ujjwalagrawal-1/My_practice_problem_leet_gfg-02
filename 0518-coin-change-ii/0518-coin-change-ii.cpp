90d84fbab7163b64e2fedc6a68229abc18ecb20b
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-17 14:33:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -27,11 +27,28 @@ public:
 
         return dp[i][amount] = a + b;
     }
+    int solve_using_tabulation(int amount,vector<int>& coins,vector<vector<int>>& dp){
+        for(int i = 0;i<dp[0].size();i++){
+            dp[0][i] = (amount%coins[0] == 0);
+        }
+        for(int it = 1;it<coins.size();it++){
+            for(int h = 0;h <= amount;h++){
+            int a = 0;
+            if(h >= coins[it])
+            a = dp[it][h-coins[it]];
+            int b = dp[it-1][h];
+            dp[it][h] = a + b;
+            }
+        }
+        return dp[coins.size()-1][amount];
+    }
     int change(int amount, vector<int>& coins) {
         int index = coins.size()-1;
         // return solve(amount,coins,index);
 
         vector<vector<int>> dp(index+1,vector<int> (amount+1,-1));
-        return solve_using_Memoisation(amount,coins,index,dp);
+        // return solve_using_Memoisation(amount,coins,index,dp);
+
+        return solve_using_tabulation(amount,coins,dp);
     }
 };
