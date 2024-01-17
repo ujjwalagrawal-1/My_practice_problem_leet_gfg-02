c6691b2d7da46e36026212b95134ed4044af04df
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-17 14:55:23 +0530

    Time: 39 ms (28.76%), Space: 18.8 MB (45.86%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -27,28 +27,28 @@ public:
 
         return dp[i][amount] = a + b;
     }
-    int solve_using_tabulation(int amount,vector<int>& coins,vector<vector<int>>& dp){
-        for(int i = 0;i<dp[0].size();i++){
-            dp[0][i] = (amount%coins[0] == 0);
+    int solve_using_tabulation(int amount, vector<int>& coins, int i, vector<vector<int>>& dp) {
+        for (int ind = 0; ind <= amount; ind++) {
+            dp[0][ind] = (ind % coins[0] == 0);
         }
-        for(int it = 1;it<coins.size();it++){
-            for(int h = 0;h <= amount;h++){
-            int a = 0;
-            if(h >= coins[it])
-            a = dp[it][h-coins[it]];
-            int b = dp[it-1][h];
-            dp[it][h] = a + b;
+        for (int ind = 1; ind < i; ind++) {
+            for (int amo = 0; amo <= amount; amo++) {
+                int a = 0;
+                if (amo >= coins[ind])
+                    a = dp[ind][amo - coins[ind]];
+                int b = dp[ind - 1][amo];
+                dp[ind][amo] = a + b;
             }
         }
-        return dp[coins.size()-1][amount];
+        return dp[i - 1][amount];
     }
     int change(int amount, vector<int>& coins) {
-        int index = coins.size()-1;
+        int index = coins.size();
         // return solve(amount,coins,index);
 
-        vector<vector<int>> dp(index+1,vector<int> (amount+1,-1));
+        vector<vector<int>> dp(index,vector<int> (amount+1,0));
         // return solve_using_Memoisation(amount,coins,index,dp);
 
-        return solve_using_tabulation(amount,coins,dp);
+        return solve_using_tabulation(amount,coins,index,dp);
     }
 };
