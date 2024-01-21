0c4fbca05492ed29bf9e97242791ecc26f077a36
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-21 10:25:56 +0530

    Time: 1 ms (40.68%), Space: 9.3 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -31,14 +31,48 @@ int memoisation(vector<int> &nums,int n,vector<int> &dp){
     dp[n] = max(ex,in);
     return dp[n];
 }
+
+int tabulation(vector<int> &nums,int n){
+    vector<int> dp(n,0);
+    dp[0] = nums[0]; 
+    // dp[1] = nums[0];
+    // 1->n;
+        for(int i = 1;i < n;i++){
+            int in = 0;
+            if(i>=2)
+            in = dp[i-2] + nums[i];
+
+            int ex = dp[i-1] + 0;
+            dp[i] = max(ex,in);
+        }
+    return dp[n-1];
+    
+}
+
+
+int spaceoptimisation(vector<int> nums){
+    int prev2 = 0;
+    int prev = nums[0];
+
+    for(int i = 1;i < nums.size();i++){
+            int in = nums[i];
+            if(i >=2)
+            in += prev2;
+
+            int ex = prev + 0;
+            int ans = max(ex,in);
+            prev2 = prev;
+            prev = ans;
+        }
+        return prev;
+}
     int rob(vector<int>& nums) {
         // vector<int> dp(nums.size()+1,0);
         // dp[1] = nums[0];
-        // dp[2] = nums[1];
+        // // dp[2] = nums[1];
         // int maxi = INT_MIN;
         // for(int j = 3;j<nums.size()+1;j++){
-        //     dp[j] = max(dp[j-2],dp[j-3]);
-        //     dp[j] += nums[j-1];
+        //     dp[j] = max(dp[j-2],dp[j-3]) + nums[j-1];
         //     // cout<<dp[j]<<" ";
         //     maxi = max(maxi,dp[j]);
         // }
@@ -52,7 +86,12 @@ int memoisation(vector<int> &nums,int n,vector<int> &dp){
 
 
         // 3rd approch;
-        vector<int> dp(nums.size()+1,-1);
-        return memoisation(nums,nums.size()-1,dp);
+        // vector<int> dp(nums.size()+1,-1);
+        // return memoisation(nums,nums.size()-1,dp);
+
+
+
+        //4th approch
+        return spaceoptimisation(nums);
     }
 };
