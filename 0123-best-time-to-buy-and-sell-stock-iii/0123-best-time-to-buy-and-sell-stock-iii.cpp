df6e65c4482cc2fbee10d91e83979023d1919f02
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-01-23 16:52:25 +0530

    Time: 423 ms (48.93%), Space: 209.6 MB (32.27%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -26,10 +26,10 @@ int using_tabulation(vector<int> &prices,vector<vector<vector<int>>> &dp){
             for(int cap = 1;cap<=2;cap++){
                 int ans = 0;
                 if(buy){
-                    ans = max(-prices[ind] + func(prices,ind+1,0,cap,dp),0 + func(prices,ind+1,1,cap,dp));
+                    ans = max(-prices[ind] + dp[ind+1][0][cap],0 + dp[ind+1][1][cap]);
                 }
                 else{
-                    ans = max(prices[ind] + func(prices,ind+1,1,cap-1,dp),0 + func(prices,ind+1,0,cap,dp));
+                    ans = max(prices[ind] + dp[ind+1][1][cap-1],0 + dp[ind+1][0][cap]);
                 }
 
                 dp[ind][buy][cap] = ans;
@@ -38,16 +38,38 @@ int using_tabulation(vector<int> &prices,vector<vector<vector<int>>> &dp){
     }
     return dp[0][1][2];
 }
+
+// int space_optimisation(vector<int> &prices){
+//     vector<vector<int>> forw(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
+//     for(int ind = prices.size()-1;ind >= 0;ind--){
+//         for(int buy = 0;buy <= 1;buy++){
+//             for(int cap = 1;cap<=2;cap++){
+//                 int ans = 0;
+//                 if(buy){
+//                     ans = max(-prices[ind] + func(prices,ind+1,0,cap,dp),0 + func(prices,ind+1,1,cap,dp));
+//                 }
+//                 else{
+//                     ans = max(prices[ind] + func(prices,ind+1,1,cap-1,dp),0 + func(prices,ind+1,0,cap,dp));
+//                 }
+
+//                 dp[ind][buy][cap] = ans;
+//             }
+//         }
+//     }
+//     return dp[0][1][2];
+// }
     int maxProfit(vector<int>& prices) {
         bool buy = 1;
         int cap = 2;
         int ind = 0;
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
         // return func(prices,ind,buy,cap,dp);
 
 
         // using tabulation
         return using_tabulation(prices,dp);
+        // tabulation -> spaceoptimisation 
+        // return space_optimisation(prices);
     }
 };
