4ecb5d5ba59d05d5d525c9ac3cb050a37200335e
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-02 00:53:16 +0530

    Time: 237 ms (50.75%), Space: 294.7 MB (6.88%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -10,14 +10,26 @@ public:
         int maxLen = 0 + solve(nums, ind + 1, p, dp);
         if (p == -1 || nums[ind] > nums[p])
             maxLen = max(maxLen, 1 + solve(nums, ind + 1, ind, dp));
-
-        return dp[ind][p + 1] = maxLen;
+            
+        dp[ind][p + 1] = maxLen;
+        return dp[ind][p+1];
     }
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return solve(nums, 0, -1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+        // return solve(nums, 0, -1, dp);
+        for(int ind = n-1;ind>=0;ind--){
+            for(int p = ind-1;p>=-1;p--){
+                int maxLen = 0 + dp[ind + 1][p+1];
+                if (p == -1 || nums[ind] > nums[p])
+                    maxLen = max(maxLen, 1 + dp[ind + 1][ind+1]);
+                    
+                dp[ind][p + 1] = maxLen;
+            }
+        }
+        return dp[0][0];
     }
+   
 
 };
