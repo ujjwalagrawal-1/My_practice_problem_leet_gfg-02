22ac920d65107fcc78f2cb046ea77b5ee8ad9af9
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-03 16:02:27 +0530

    Time: 20 ms (96.41%), Space: 12.2 MB (15.16%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -19,7 +19,23 @@ int f(int i,int j,vector<int>& cuts,vector<vector<int>> &dp){
         cuts.push_back(n);
         cuts.insert(cuts.begin(),0);
         sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
-        return f(1,c,cuts,dp);
+        vector<vector<int>> dp(c+2,vector<int>(c+2,0));
+        // return f(1,c,cuts,dp); solved using Memoisation
+        // below is using tabulation 
+
+        for(int i = c;i>=1;i--){
+            for(int j = 1;j<=c;j++){
+                if(i>j){
+                    continue;
+                }
+                int mini = INT_MAX;
+                for(int ind = i;ind<=j;ind++){
+                    int ans = cuts[j+1]-cuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = min(ans,mini);
+                }
+                dp[i][j] =  mini;
+            }
+        }
+        return dp[1][c];
     }
 };
