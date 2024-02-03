b47ab51e4f1d27bad135ba5964b437210b38064b
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-03 18:29:09 +0530

    Time: 3 ms (99.46%), Space: 10.7 MB (26.98%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int f(vector<int>& arr, int k, int ind,int n,vector<int> &dp) {
+    int f(vector<int>& arr, int k, int ind, int n, vector<int>& dp) {
         if (ind >= arr.size()) {
             return 0;
         }
-        if(dp[ind] != -1){
+        if (dp[ind] != -1) {
             return dp[ind];
         }
         int maxi = INT_MIN;
@@ -13,14 +13,27 @@ public:
         for (int i = ind; i < min(ind + k, n); i++) {
             maxb = max(maxb, arr[i]);
             len++;
-            int sum = (maxb * len) + f(arr, k, i + 1,n,dp);
+            int sum = (maxb * len) + f(arr, k, i + 1, n, dp);
             maxi = max(maxi, sum);
         }
         return dp[ind] = maxi;
     }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(),-1);
-        return f(arr, k, 0,arr.size(),dp);
+        vector<int> dp(arr.size()+1, 0);int n = arr.size();
+        // return f(arr, k, 0, arr.size(), dp); solved using Memoisation
+        for(int ind = n-1;ind >= 0;ind--){
+        int maxi = INT_MIN;
+        int maxb = INT_MIN;
+        int len = 0;
+        for (int i = ind; i < min(ind + k, n); i++) {
+            maxb = max(maxb, arr[i]);
+            len++;
+            int sum = (maxb * len) + dp[i + 1];
+            maxi = max(maxi, sum);
+        }
+        dp[ind] = maxi;
+        }
+        return dp[0];
     }
 };
