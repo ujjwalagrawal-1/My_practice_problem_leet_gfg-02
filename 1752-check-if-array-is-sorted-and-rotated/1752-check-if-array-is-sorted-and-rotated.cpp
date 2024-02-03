1f48bfee566f9afb33b7b49c53a0236e871a88b4
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-04 02:55:41 +0530

    Time: 0 ms (100%), Space: 10.2 MB (15.2%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -12,9 +12,7 @@ public:
         }
         int i = 0;
         while(i < nums.size()-1){
-            // cout<<(i+ind)%nums.size()<<endl;
             if(nums[(i+ind)%nums.size()] > nums[(i+ind+1)%nums.size()]){
-                // cout<<"yes"<<endl;
                 return false;
             }
             i++;
