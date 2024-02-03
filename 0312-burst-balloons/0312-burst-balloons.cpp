e0408dd7a15f9018122ef3c680f7025b1d86224f
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-03 16:46:58 +0530

    Time: 113 ms (91.33%), Space: 12 MB (9.31%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -18,12 +18,22 @@ int f(int i,int j,vector<int> &nums,vector<vector<int>> &dp){
         int s = nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>> dp(s+1,vector<int>(s+1,-1));
-        return f(1,s,nums,dp); 
-        // for(int i = s;i>=1;i--){
-        //     for(int j = 1;j<=s;j++){
+        vector<vector<int>> dp(s+2,vector<int>(s+2,0));
+        // return f(1,s,nums,dp); solved using Memoisation
 
-        //     }
-        // }
+        for(int i = s;i>=1;i--){
+            for(int j = 1;j<=s;j++){
+                if(i>j){
+                    continue;
+                }
+                int maxi = INT_MIN;
+                for(int ind = i;ind<=j;ind++){
+                    int ans = nums[i-1]*nums[ind]*nums[j+1] +dp[i][ind-1] + dp[ind+1][j];
+                    maxi = max(ans,maxi);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][s];
     }
 };
