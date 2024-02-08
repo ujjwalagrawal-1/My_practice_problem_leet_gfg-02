44a9234c352c5a449f32ca646a050133b3bdef6f
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-02-08 14:48:08 +0530

    Time: 0 ms (100%), Space: 7.3 MB (15.62%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-int solve(int n){
+int solve(int n,vector<int>& dp){
     if(n == 0 || n == 1){
         return 1;
     }
+    if(dp[n]!=-1)return dp[n];
     int ans = 0;
     for(int i = 1;i<=n;i++){
-        ans += (solve(i-1)*solve(n-i));
+        ans += (solve(i-1,dp)*solve(n-i,dp));
     }
-    return ans;
+    return dp[n] = ans;
 }
     int numTrees(int n) {
       //  dp on trees
-        return solve(n); 
+      vector<int> dp(n+1,-1);
+      return solve(n,dp); 
     }
 };
