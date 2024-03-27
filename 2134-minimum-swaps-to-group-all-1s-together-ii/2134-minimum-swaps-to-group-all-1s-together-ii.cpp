df73723fc4c7d560a6363ae65d4243494508b050
ujjwalagrawal-1 <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-03-27 16:11:14 +0530

    Time: 70 ms (51.57%), Space: 87.4 MB (38.33%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int cnt = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == 1){
+                cnt++;
+            }
+        }
+        int k = cnt;
+        int i = 0;
+        while(k--){
+            nums.push_back(nums[i]);
+            i++;
+        }
+        int mini  = INT_MAX;
+        int zero = 0;
+        k = cnt;
+        i = 0;int j = 0;
+        while(j<nums.size()){
+            if(nums[j] == 0){
+                zero++;
+            }
+            if(j-i+1 > k){
+                if(nums[i] == 0){
+                    zero--;
+                }
+                i++;
+            }
+            if(j-i+1 == k){
+                // cout<<mini<<endl;
+                mini = min(mini,zero);
+            }
+            j++;
+        }
+        return mini ;
+    }
+};
