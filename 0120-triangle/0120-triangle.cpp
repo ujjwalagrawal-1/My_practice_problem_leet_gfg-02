a79e7fecba683d51b59389c31f48abc41e1f42b5
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-05-16 03:24:20 +0530

    Time: 4 ms (72.84%), Space: 11.1 MB (73.74%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -13,9 +13,25 @@ int f(vector<vector<int>> &triangle,int i,int j,vector<vector<int>> &dp){
     return dp[i][j] =  min(f(triangle,i+1,j,dp),f(triangle,i+1,j+1,dp)) + triangle[i][j];
 }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int i = 0;
-        int j = 0;
-        vector<vector<int>> dp(triangle.size(),vector<int>(triangle.size(),-1));
-        return f(triangle,i,j,dp);
+        // int i = 0;
+        // int j = 0;
+        // vector<vector<int>> dp(triangle.size()+1,vector<int>(triangle.size()+1,0));
+        vector<int> prev(triangle.size()+1,0);
+        vector<int> curr(triangle.size()+1,0);
+        // return f(triangle,i,j,dp);  // Solved Using Recursion 
+        for(int i = 0;i<triangle.size();i++){
+            for(int j = i+1;j<=triangle.size();j++){
+                prev[j] = INT_MAX;
+            }
+        }
+
+        for(int i = triangle.size()-1;i>=0;i--){
+            for(int j = i;j>=0;j--){
+                prev[j] =  min(curr[j],curr[j+1]) + triangle[i][j];
+            }
+            curr = prev;
+        }
+
+        return curr[0];
     }
 };
