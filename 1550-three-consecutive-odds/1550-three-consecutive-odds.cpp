02dafd8883b2a4cb02357054c9829a912879cc5e
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-07-02 00:58:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        for(int i = 0;i<arr.size()-3;i++){
+            if(arr[i]%2 == arr[i+1]%2 == arr[i+2]%2 == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
