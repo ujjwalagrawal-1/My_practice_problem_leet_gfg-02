9312deefa2ada8ca9b14d568b251f912543307fe
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-07-16 22:44:28 +0530

    Time: 11 ms (52.99%), Space: 22.8 MB (42.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    int trap(std::vector<int>& ht) {
-        stack<int> st;
-        st.push(INT_MIN);
-        vector<int> veci(ht.size());
-        vector<int> veci2(ht.size());
-        int i = 0;
-        while(i<ht.size()){
-            if(!st.empty() && st.top() < ht[i]){
-                st.push(ht[i]);
-            }
-            veci[i] = st.top();
-            i++;
-        }
-        while(!st.empty()){
-            st.pop();
-        }
-        st.push(INT_MIN);
-        i = ht.size()-1;
-        for(auto itr:veci){
-            cout<<itr<<" ";
-        }cout<<endl;
-        while(i >= 0){
-            if(!st.empty() && st.top() < ht[i]){
-            st.push(ht[i]);
-            }
-            veci2[i] = st.top();
-            i--;
-        }
+    int trap(vector<int>& h) {
+        int l = 0;
+        int r = h.size()-1;
+        int lft = 0;int rgt = 0;
         int ans = 0;
-        for(int i = 0;i<veci.size();i++){
-            ans += min(veci[i],veci2[i]) >= ht[i] ?  min(veci[i],veci2[i])- ht[i]:0;
+        while(l<r){
+            if(h[l] <= h[r]){
+                if(lft < h[l])lft = h[l];
+                else{
+                    ans += lft - h[l];
+                    l++;
+                }
+            }
+            else{
+                if(rgt < h[r]){
+                    rgt = h[r];
+                }
+                else{
+                    ans += rgt - h[r];
+                    r--;
+                }
+            }
         }
-
         return ans;
-
     }
 };
