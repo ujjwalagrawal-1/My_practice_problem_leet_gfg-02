d23898c8859f63dc41cfef6f95a08c61ef3f1352
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-07-30 15:36:01 +0530

    Time: 108 ms (33.97%), Space: 24.5 MB (56.62%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        stack<char> st;
+        int cnt = 0;
+        for(int i = 0;i<s.size();i++){
+            if(!st.empty() && st.top() == 'b' && s[i] == 'a'){
+                st.pop();
+                cnt++;
+                continue;
+            }
+            else{
+                st.push(s[i]);
+            }
+        }
+        return cnt;
+    }
+};
