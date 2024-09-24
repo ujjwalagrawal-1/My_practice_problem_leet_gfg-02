3b688b7eb8375663a3e7614d5f8937a61e0be6a2
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-09-24 13:20:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,36 +1,43 @@
-struct Node{
-    Node* links[10];
-};
 class Solution {
 public:
-void createTrie(vector<int> &arr1,Node* &root){
-    for(auto itr : arr1){
-        Node* temp = root;
-        string r = to_string(itr);
-        for(int i = 0;i<r.size();i++){
-            if(temp->links[r[i] - '0'] == NULL){
-                temp->links[r[i] - '0'] = new Node();
-            }
-            temp = temp->links[r[i] - '0'];
-        }
-    }
-}
-int traverse_find_the_Max(Node* a,Node* b){
-    int maxi = 0;
-    for(int i = 0;i<10;i++){
-        if(a->links[i] && b->links[i]){
-            maxi = max(maxi,traverse_find_the_Max(a->links[i],b->links[i]) + 1);
-        }
-    }
-    return maxi;
-}
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        Node* a = new Node();
-        Node* b = new Node();
-        //  CREATED Trie 
-        createTrie(arr1,a);
-        createTrie(arr2,b);
-        // Now Search inside the Trie;
-        return traverse_find_the_Max(a,b);
+        map<string,int> mp1;
+        map<string,int> mp2;
+        for(int i = 0;i<arr1.size();i++){
+            mp1[to_string(arr1[i])]++;
+        }
+
+        for(int i = 0;i<arr2.size();i++){
+            mp2[to_string(arr2[i])]++;
+        }
+        int maxi = INT_MIN;
+        for(int i = 0;i<arr1.size();i++){
+            string temp = "";
+            string move = to_string(arr1[i]);
+            for(int j = 0;j<move.size();j++){
+                temp += move[j];
+                if(mp2[temp] != 0){
+                    maxi = max(maxi,j+1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        for(int i = 0;i<arr2.size();i++){
+            string temp = "";
+            string move = to_string(arr2[i]);
+            for(int j = 0;j<move.size();j++){
+                temp += move[j];
+                if(mp1[temp] != 0){
+                    maxi = max(maxi,j+1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+
+        return maxi == INT_MIN ? 0 : maxi;
     }
 };
