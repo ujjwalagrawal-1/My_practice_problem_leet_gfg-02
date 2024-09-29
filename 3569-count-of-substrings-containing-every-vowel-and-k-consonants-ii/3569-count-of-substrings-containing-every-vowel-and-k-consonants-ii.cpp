775a4730d82e5299225bc35243334c97dceb36d6
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-09-30 00:48:31 +0530

    Time: 212 ms (100%), Space: 40.7 MB (100%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -2,67 +2,38 @@ class Solution {
 public:
 bool is(char &ch){
     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-        return 1;
+        return true;
     }
-    return 0;
+    return false;
 }
+long long f(string word,int k){
+    vector<int> f(26,0);
+    long long ans = 0;
+    int i = 0,j = 0;
+    long long cnt = 0;
+    while(j < word.size()){
+        if(is(word[j])){
+            f[word[j] - 'a']++;
+        }
+        else if(!is(word[j])){
+            cnt++;
+        }
+        while(i<=j && cnt >= k && f[int('a' - 'a')] > 0 && f[int('e' - 'a')] > 0 && f[int('i' - 'a')] > 0  && f[int('o' - 'a')] > 0 && f[int('u' - 'a')] > 0){
+            ans += (word.size() - j);
+            if(is(word[i])){
+                f[word[i] - 'a']--;
+            }
+            else if(!is(word[i])){
+                cnt--;
+            }
+            i++;
+        }
+        j++;
+    }
 
-int chk(vector<int> &f,int k){
-    int cnt = 0;
-    bool fl = 0;
-    if(f['a' - 'a'] > 0 && f['e' - 'a'] > 0 && f['i' - 'a'] > 0 && f['o' - 'a'] > 0 && f['u' - 'a'] > 0){
-        fl = 1;
-    }
-    for(int i = 0;i<26;i++){
-        if(f[i] > 0){
-            if(i == int('a' - 'a') || i == int('e' - 'a') || i == int('i' - 'a') || i == int('o' - 'a') || i == int('u' - 'a')){
-            }
-            else{
-                cnt += f[i];
-            }
-        }
-    }
-        if(fl && cnt == k){
-            // cout<<"1 is out  "<<endl;
-            return 1;
-        }
-        if(fl && cnt > k){
-            // cout<<"2 is out "<<endl;
-            return 2;
-        }
-        return 0;
+    return ans;
 }
-    long long countOfSubstrings(string w, int k) {
-        long long ans = 0;
-        int n = w.size();
-        vector<int> veci(n,n);
-        int lc = n;
-        for(int i = n-1;i >= 0;i--){
-            veci[i] = lc;
-            if(!is(w[i])){
-                lc = i;
-            }
-        }
-        vector<int> f(26,0);
-        int i = 0,j = 0;
-        while(j < veci.size()){
-            f[w[j] - 'a']++;
-            int val = chk(f,k);
-            while(i <= j && val >= 1){
-                if(val == 1){
-                    ans += (veci[j] - j);
-                    f[w[i] - 'a']--;
-                    i++;
-                    val = chk(f,k);
-                }
-                else{
-                    f[w[i] - 'a']--;
-                    i++;
-                    val = chk(f,k);
-                }
-            }
-            j++;
-        }
-        return ans;
+    long long countOfSubstrings(string word, int k) {
+        return f(word,k) - f(word,k+1);
     }
 };
