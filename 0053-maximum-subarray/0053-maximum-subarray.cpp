2c1601edd8bdb7eb656f88e5fe8de34566129c88
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-11-14 15:30:15 +0530

    Time: 0 ms (100%), Space: 70.5 MB (89.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    int f(vector<int> &nums, int i,vector<int> &dp) {
-        if (i < 0) {
-            return 0;
-        }
-        if(dp[i]!=-1)return dp[i];
-        int not_taken = nums[i];
-        int taken = nums[i] + f(nums,i-1,dp) ; 
-        return dp[i] = max(not_taken,taken);
-    }
     int maxSubArray(vector<int>& nums) {
-        int maxi = INT_MIN;vector<int> dp(nums.size(),-1);
-        for(int i = nums.size()-1;i>=0;i--){
-            maxi = max(maxi,f(nums,i,dp));
+        int ans = INT_MIN;
+        int sum = 0;
+        int i = 0;
+        while(i<nums.size()){
+            sum += nums[i];
+            ans = max(ans,sum);
+            if(sum < 0){
+                sum = 0;
+            }
+            i++;
         }
-        return maxi;
+        return ans;
     }
 };
