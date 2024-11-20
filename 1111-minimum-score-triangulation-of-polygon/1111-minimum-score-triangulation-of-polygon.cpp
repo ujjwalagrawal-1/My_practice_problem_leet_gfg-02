adabbdda8454a0dc0c1a81cf0a0489225d9a7d14
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2024-11-20 22:15:53 +0530

    Time: 5 ms (11.85%), Space: 10.5 MB (8.93%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-int f(vector<int>& values,int i,int n,vector<vector<int>> &dp){
-    if(i == n){
+long long f(vector<int> &veci,int i,int j,vector<vector<long long>> & dp){
+    if(i == j){
         return 0;
     }
-    if(dp[i][n] != -1){
-        return dp[i][n];
+    if(dp[i][j] != -1){
+        return dp[i][j];
     }
     long long mini = INT_MAX;
-    for(int k = i;k<=n-1;k++){
-        mini = min(mini,f(values,i,k,dp) + f(values,k+1,n,dp) + 1ll*values[i-1]*values[k]*values[n]);
+    for(int k = i;k <= j-1;k++){
+        mini = min(mini,f(veci,i,k,dp) + f(veci,k+1,j,dp) + 1ll*veci[i-1]*veci[k]*veci[j]);
     }
-
-    return dp[i][n] = mini;
+    return dp[i][j] =  mini;
 }
     int minScoreTriangulation(vector<int>& values) {
-        vector<vector<int>> dp(values.size(),vector<int>(values.size(),-1));
-        return f(values,1,values.size()-1,dp);
+        int n = values.size();
+        vector<vector<long long>> dp(n+1,vector<long long> (n+1, 0));
+
+        for(int i = n-2;i >= 1;i--){
+            for(int j = 2;j<= n-1;j++){
+                if(i == j){
+                    continue;
+                }
+                long long mini = INT_MAX;
+                for(int k = i;k <= j-1;k++){
+                    mini = min(mini,f(values,i,k,dp) + f(values,k+1,j,dp) + 1ll*values[i-1]*values[k]*values[j]);
+                }
+                dp[i][j] =  mini;
+            }
+        }
+
+        return dp[1][n-1];
+        // return f(values,1,n-1,dp);
     }
 };
