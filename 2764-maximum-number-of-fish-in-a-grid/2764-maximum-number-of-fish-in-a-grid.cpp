1b816cf928ff70be67af3181fca1db86d91cfec7
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-01-28 08:16:44 +0530

    Time: 6 ms (69%), Space: 94 MB (94.22%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-int dfs(int i,int j,vector<vector<int>> & vis,vector<vector<int>> &grid){
-    vis[i][j] = 1;
+int dfs(int i,int j,vector<vector<int>> &grid){
     int dx[4] = {1,0,-1,0};
     int dy[4] = {0,-1,0,1};
     int ans = grid[i][j];
+    grid[i][j] = 0;
     for(int k = 0;k<4;k++){
         int nx = i + dx[k];
         int ny = j + dy[k];
-        if(nx < vis.size() &&  nx >=0 && ny < vis[0].size() && ny >= 0 && grid[nx][ny] > 0 && !vis[nx][ny]){
-            ans += dfs(nx,ny,vis,grid);
+        if(nx < grid.size() &&  nx >=0 && ny < grid[0].size() && ny >= 0 && grid[nx][ny] > 0){
+            ans += dfs(nx,ny,grid);
         }
     }
     return ans;
 }
     int findMaxFish(vector<vector<int>>& grid) {
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
+        // vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),0));
         int maxi = 0;
         for(int i = 0;i<grid.size();i++){
             for(int j =  0 ;j<grid[0].size();j++){
-                if(!vis[i][j] && grid[i][j] > 0){
-                    maxi = max(maxi,dfs(i,j,vis,grid));
+                if( grid[i][j] > 0){
+                    maxi = max(maxi,dfs(i,j,grid));
                 }
             }
         }
