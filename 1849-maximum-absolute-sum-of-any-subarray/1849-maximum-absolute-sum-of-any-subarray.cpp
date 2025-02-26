8c11597a4134412678682e325b9480d78f362e41
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-02-27 02:05:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int maxi = INT_MIN,mini = INT_MAX,cmax_sum = 0,cmin_sum = 0;
+        for(int i = 0;i < nums.size();i++){
+            cmax_sum += nums[i];
+            cmin_sum += nums[i];
+            maxi = max(maxi,cmax_sum);
+            mini = min(mini,cmin_sum);
+            if(cmax_sum <= 0)cmax_sum = 0;
+            if(cmin_sum >= 0)cmin_sum = 0;
+        }
+        return max(cmax_sum,abs(cmin_sum)) + 1;
+    }
+};
