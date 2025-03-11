a07528c9587b13be1ea09bae2477eee95f431e1f
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-03-11 10:00:53 +0530

    Time: 43 ms (9.68%), Space: 21.1 MB (5.04%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,27 +1,23 @@
-#include <unordered_map>
-#include <string>
-
 class Solution {
 public:
-    long long numberOfSubstrings(std::string s) {
-        unordered_map<char, int> mp;
-        int n = s.size();
-        int i = 0, j = 0;
-        long long count = 0;
+    int numberOfSubstrings(string s) {
+        int i = 0;
+        int j = 0;
+        int ans = 0;
 
-        while (j < n) {
+        map<char,int> mp;
+        while(j < s.size()){
             mp[s[j]]++;
-            
-            while (mp.size() == 3) {
-                count += (n - j);
+            while(mp.size() == 3){
+                ans += (s.size() - j);
                 mp[s[i]]--;
-                if (mp[s[i]] == 0) {
+                if(mp[s[i]] == 0){
                     mp.erase(s[i]);
                 }
                 i++;
             }
             j++;
         }
-        return count;
+        return ans;
     }
 };
