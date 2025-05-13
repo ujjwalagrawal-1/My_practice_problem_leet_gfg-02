7c605e23ffac282e2168bf1407c2c2d9bd32c86c
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-05-13 22:45:46 +0530

    Time: 184 ms (16.36%), Space: 41.3 MB (21.82%) - LeetHub

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static bool comp(vector<int> & a ,vector<int> &b){
+        if(a[0] == b[0]){
+            return a[1] > b[1];
+        }
+        return a[0] < b[0];
+    }
+    int numberOfPairs(vector<vector<int>>& p) {
+        sort(p.begin(),p.end(),comp);
+        for(int i = 0;i<p.size();i++){
+            cout<<p[i][0]<<" "<<p[i][1]<<endl;
+        }
+        int cnt = 0;
+        for(int i = 0;i<p.size()-1;i++){
+            int y = INT_MIN;
+            for(int j = i+1;j<p.size();j++){
+                if(p[i][1] >= p[j][1]){
+                    // cout<<"yes for i , j "<<i<<" "<<j<<endl;
+                    if(y < p[j][1]){
+                        // cout<<"no for i , j "<<i<<" "<<j<<endl;
+                        cnt++;
+                    }
+                    y = max(y,p[j][1]);
+                }
+            }
+        }
+        return cnt;
+    }
+};
