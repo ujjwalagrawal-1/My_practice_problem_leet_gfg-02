c61bfddb3b41094fd2df5f09da4ecf834c1b9cec
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-05-17 02:03:21 +0530

    Time: 110 ms (97.32%), Space: 79.5 MB (94.64%) - LeetHub

--- 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.cpp
+++ 2339-k-divisible-elements-subarrays/2339-k-divisible-elements-subarrays.cpp
@@ -1,20 +1,26 @@
+struct node{
+    map<int,node*> links;
+};
 class Solution {
 public:
     int countDistinct(vector<int>& nums, int k, int p) {
-        set<vector<int>> st;
+        node* root = new node();
         int ans = 0;
         for(int i = 0;i<nums.size();i++){
+            node* temp = root;
             int cnt = 0;
-            vector<int> veci;
             for(int j = i;j<nums.size();j++){
                 if(nums[j]%p == 0)cnt++;
                 if(cnt > k){
                     break;
                 }
-                veci.push_back(nums[j]);
-                st.insert(veci);
+                if(temp->links[nums[j]] == NULL){
+                    temp->links[nums[j]] = new node();
+                    ans++;
+                }
+                temp = temp->links[nums[j]];
             }
         }
-        return st.size();
+        return ans;
     }
 };
