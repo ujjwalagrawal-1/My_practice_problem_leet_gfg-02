55067a0816fc6ca8f12d8a9f13e788a94e32293d
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-05-28 23:51:48 +0530

    Time: 0 ms (100%), Space: 23.7 MB (43.25%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,4 +1,4 @@
- /**
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -11,42 +11,18 @@
  */
 class Solution {
 public:
-    // int maxDepth(TreeNode* root) {
-    //     if(!root){
-    //         return 0;
-    //     }
-    //     int rootkeleft = maxDepth(root->left);
-    //     int rootkeright = maxDepth(root->right);
-    //     int ans = max(rootkeleft,rootkeright) + 1;
-    //     return ans;
-    // }
-    // int diameterOfBinaryTree(TreeNode* root) {
-    //     if(root == NULL){
-    //         return 0;
-    //     }
-    //     int lefti = diameterOfBinaryTree(root->left);
-    //     int righti = diameterOfBinaryTree(root->right);
-    //     int comb = maxDepth(root->left) + maxDepth(root->right);
-    //     int ans = max(lefti,max(righti,comb));
-    //     return ans;
-    // }
-
-
-    // the above way to find the Diamete of the tree is very large way Below is the fasted way to find the Diamete of the tree
-    int D = 0;
-    int maxDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-        int rootkeleft = maxDepth(root->left);
-        int rootkeright = maxDepth(root->right);
-        int x = rootkeleft + rootkeright;
-        D = max(D,x);
-        int ans = max(rootkeleft,rootkeright) + 1;
-        return ans;
+int f(TreeNode* &root,int &ans){
+    if(root == NULL){
+        return 0;
     }
+    int l = f(root->left,ans);
+    int r = f(root->right,ans);
+    ans = max(ans,l + r);
+    return max(l,r) + 1;
+}
     int diameterOfBinaryTree(TreeNode* root) {
-        maxDepth(root);
-        return(D);
+        int ans = 0;
+        f(root,ans);
+        return ans;
     }
 };
