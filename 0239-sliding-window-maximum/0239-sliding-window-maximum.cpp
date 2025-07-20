b8536c4e5f245b7d31ef1c7dbb1a0dafc29e5e4b
Ujjwal Agrawal <122930278+ujjwalagrawal-1@users.noreply.github.com>
2025-07-20 23:50:47 +0530

    Time: 32 ms (33.26%), Space: 139.1 MB (83.52%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -3,26 +3,17 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         deque<int> qu;
         vector<int> ans;
-        int j = 0,i = 0;
-        // qu.push(nums[0]);
-        
-        while(j<nums.size()){
-            while(!qu.empty() && qu.back()<nums[j]){
+        for(int i = 0;i<nums.size();i++){
+            while(!qu.empty() && qu.front() <= i-k){
+                qu.pop_front();
+            }
+            while(!qu.empty() && nums[qu.back()] < nums[i]){
                 qu.pop_back();
             }
-            qu.push_back(nums[j]);
-            if(j-i+1<k){
-                j++;
+            qu.push_back(i);
+            if(i >= k-1){
+                ans.push_back(nums[qu.front()]);
             }
-            else if(j-i+1 == k){
-                ans.push_back(qu.front());
-                if(nums[i] == qu.front()){
-                    qu.pop_front();
-                }
-                i++;
-                j++;
-            }
-
         }
         return ans;
     }
